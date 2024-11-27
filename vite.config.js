20b79b81840acba29ff0c8944af791c601567067
HuyTran <huy.tran@lundimatin.fr>
2024-11-27 21:48:14 +0700

    Fix deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 10000,
+        host: true,
+        strictPort: true,
+        port: 8000,
     },
 })
