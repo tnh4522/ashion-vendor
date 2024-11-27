878ccc6ca9a19724dfe7bf597c5ec7d050f3f486
HuyTran <huy.tran@lundimatin.fr>
2024-11-27 21:13:43 +0700

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,30 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'D:\\ashionEcommerceApi\\static',
-    manifest: "manifest.json",
-    rollupOptions: {
-      input: [
-        'src/main.jsx',
-      ]
-    }
-  }
+    plugins: [react()],
+    build: {
+        outDir: 'D:\\ashionEcommerceApi\\static',
+        manifest: "manifest.json",
+        rollupOptions: {
+            input: [
+                'src/main.jsx',
+            ]
+        }
+    },
+    server: {
+        proxy: {
+            '/ngrok': {
+                target: 'https://sterling-notably-monster.ngrok-free.app',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/ngrok/, ''),
+                configure: (proxy) => {
+                    proxy.on('proxyReq', (proxyReq) => {
+                        proxyReq.setHeader('ngrok-skip-browser-warning', 'true');
+                    });
+                },
+            },
+        },
+    },
 })
-
