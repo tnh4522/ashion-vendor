eab5a3c3d8b1a21f8b50917597a88d597aef5525
HuyTran <huy.tran@lundimatin.fr>
2024-11-27 21:14:31 +0700

    Change API

--- src/service/service.jsx
+++ src/service/service.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/";
+const API_URL = "https://sterling-notably-monster.ngrok-free.app/api/";
 
 const API = axios.create({
     baseURL: API_URL
