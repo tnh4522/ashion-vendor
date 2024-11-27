298874524035db1db9c19f330cb38cdb69d65139
HuyTran <huy.tran@lundimatin.fr>
2024-11-27 22:16:39 +0700

    Fix deploy

--- src/service/service.jsx
+++ src/service/service.jsx
@@ -3,7 +3,10 @@ import axios from "axios";
 const API_URL = "https://sterling-notably-monster.ngrok-free.app/api/";
 
 const API = axios.create({
-    baseURL: API_URL
+    baseURL: API_URL,
+    headers: {
+        'ngrok-skip-browser-warning': 'true',
+    },
 });
 
 export default API;
