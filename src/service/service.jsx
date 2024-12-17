e41c7dddec27fb540f239b9fcfdc0eb081aa78aa
HuyTran <huy.tran@lundimatin.fr>
2024-12-18 00:23:31 +0700

    Change Endpoint API Server

--- src/service/service.jsx
+++ src/service/service.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://sterling-notably-monster.ngrok-free.app/api/";
+const API_URL = "https://included-sheepdog-slowly.ngrok-free.app/api/";
 
 const API = axios.create({
     baseURL: API_URL,
